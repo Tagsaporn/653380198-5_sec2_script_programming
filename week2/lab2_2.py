3cd56c01de0213f6a7670e92a34d4565645a5e07
Tagsaporn <tagsaporn.m@kkumail.com>
2026-06-30 08:40:14 +0700

    Create lab2_2.py

---
+++ week2/lab2_2.py
@@ -0,0 +1,15 @@
+#แก้ไข เขียนโปรแกรมให้สามารถรับค่าอายุของผู้ใช้และแสดงข้อความตามช่วงอายุที่กำหนด
+
+age = int(input("Enter your age: "))
+if age < 5:
+    print("")
+elif age <= 12:
+    print("")
+elif age <= 17:
+    print("")
+else:
+    print("")
+
+like = input("Do you like action movies? (yes/no): ").strip().lower()
+if like == "yes":
+    print("")
